3a3df6c032802e2e171a43108fcbed4755bd97fe
JoJoJet <joe102000@gmail.com>
2022-08-15 03:18:53 -0400

    Add `LeakyBox` variant, which does not get dropped

--- src/lib.rs
+++ src/lib.rs
@@ -54,5 +54,45 @@ impl ErasedBox {
     }
 }
 
+pub struct LeakyBox {
+    ptr: NonNull<u8>,
+}
+
+impl<T: 'static> From<Box<T>> for LeakyBox {
+    fn from(boxed: Box<T>) -> Self {
+        let ptr = Box::into_raw(boxed) as *mut u8;
+        Self {
+            // SAFETY: `ptr` was obtained from a `Box`, so it is guaranteed to be non-null.
+            ptr: unsafe { NonNull::new_unchecked(ptr) },
+        }
+    }
+}
+
+impl LeakyBox {
+    pub fn new<T: 'static>(val: T) -> Self {
+        Self::from(Box::new(val))
+    }
+
+    /// # Safety
+    /// This instance must have been created from a value of type `T`.
+    pub unsafe fn downcast_unchecked<T: 'static>(self) -> Box<T> {
+        // SAFETY: `self.ptr` was originally obtained from `Box::into_raw`,
+        // and the caller has promised that the type matches.
+        Box::from_raw(self.ptr.as_ptr() as *mut T)
+    }
+
+    /// # Safety
+    /// This instance must have been created from a value of type `T`.
+    pub unsafe fn downcast_ref_unchecked<T: 'static>(&self) -> &T {
+        &*(self.ptr.as_ptr() as *mut T)
+    }
+
+    /// # Safety
+    /// This instance must have been created from a value of type `T`.
+    pub unsafe fn downcast_mut_unchecked<T: 'static>(&mut self) -> &mut T {
+        &mut *(self.ptr.as_ptr() as *mut T)
+    }
+}
+
 #[cfg(test)]
 mod tests {}
